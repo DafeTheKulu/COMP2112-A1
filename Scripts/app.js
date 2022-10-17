0b8dca27499fbefda4738c758eb8daf43f14dbf3
David Janssen <djanssen2112@gmail.com>
2022-10-16 22:45:26 -0400

    added footer and icons

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,15 +2,22 @@
 (function () {
     //Function to load Header navigation
     function LoadHeader() {
-        console.log("Loading Header...");
+        // console.log("Loading Header...");
         $.get("./Views/components/header.html", function (html_data) {
             $("header").html(html_data);
         });
     }
+    // Function to load the footer
+    function LoadFooter() {
+        $.get("./Views/components/footer.html", function (html_data) {
+            $("footer").html(html_data);
+        });
+    }
     // Function to begin loading page data.
     function Start() {
-        console.log("Starting...");
+        // console.log("Starting...");
         LoadHeader();
+        LoadFooter();
     }
     window.addEventListener("load", Start);
 })();
--- Scripts/app.ts
+++ Scripts/app.ts
@@ -3,16 +3,24 @@
 (function () {
 	//Function to load Header navigation
 	function LoadHeader(): void {
-		console.log("Loading Header...");
+		// console.log("Loading Header...");
 		$.get("./Views/components/header.html", function (html_data) {
 			$("header").html(html_data);
 		});
 	}
 
+	// Function to load the footer
+	function LoadFooter(): void {
+		$.get("./Views/components/footer.html", function (html_data) {
+			$("footer").html(html_data);
+		});
+	}
+
 	// Function to begin loading page data.
 	function Start() {
-		console.log("Starting...");
+		// console.log("Starting...");
 		LoadHeader();
+		LoadFooter();
 	}
 
 	window.addEventListener("load", Start);
